718f1acccc0f0ea3ed4157aba7d2cbafefe69fd7
LOLA MATILDA NIETO <48588470@ort>
2024-10-16 13:57:09 -0300

    Creando proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoIntegrador_MiCiudad.Models;
+
+namespace ProyectoIntegrador_MiCiudad.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoIntegrador_MiCiudad.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
