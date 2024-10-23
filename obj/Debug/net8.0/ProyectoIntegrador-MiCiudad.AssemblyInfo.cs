6beba62ca2d31c04dc053be9c6a375b626f25326
FEDERICO KUROZWAN <48464810@ort>
2024-10-23 13:35:36 -0300

    cambios

--- obj/Debug/net8.0/ProyectoIntegrador-MiCiudad.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoIntegrador-MiCiudad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a785ea5b66e6df1491b6e06aec9b3883884ad68a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504f6c62b676daa598f0792b367f71050dc68002")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
