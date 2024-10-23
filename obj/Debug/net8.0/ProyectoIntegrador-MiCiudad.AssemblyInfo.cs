504f6c62b676daa598f0792b367f71050dc68002
FEDERICO KUROZWAN <48464810@ort>
2024-10-23 12:12:26 -0300

    Comenzando el .js

--- obj/Debug/net8.0/ProyectoIntegrador-MiCiudad.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoIntegrador-MiCiudad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a3bc3de275ad6a9c21ae2153813026610da393")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a785ea5b66e6df1491b6e06aec9b3883884ad68a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function ValidarLogIn(){
+    const username = documnet.getElementById('usuario');
+    const password = document.getElementById('password');
+    
+}
