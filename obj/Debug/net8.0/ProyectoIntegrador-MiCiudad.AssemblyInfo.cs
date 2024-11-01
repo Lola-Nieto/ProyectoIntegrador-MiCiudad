a85685994e83e125ed902cd1435e62890a68f910
LOLA MATILDA NIETO <48588470@ort>
2024-11-01 08:00:25 -0300

    Más cambios

--- obj/Debug/net8.0/ProyectoIntegrador-MiCiudad.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoIntegrador-MiCiudad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5467b2d8e8fdb9ed7e62b02d06d71d7a6a5d2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8209fb09fbca9733a921e8b58fa53cb1f709761e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
