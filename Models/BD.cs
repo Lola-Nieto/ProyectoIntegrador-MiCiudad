0787f0007c412e61175bf5d43f739ee29a85d462
Lola-Nieto <Lola Nieto>
2024-11-01 17:19:27 -0300

    Avanzando

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,6 +1,7 @@
 using System.Data.SqlClient;
 using Dapper;
 using System.Collections.Generic;
+using Microsoft.Extensions.ObjectPool;
 
 
 namespace ProyectoIntegrador_MiCiudad.Models;
@@ -22,10 +23,28 @@ public static bool ChequearCuentaExiste(string usuarioIngresado, string contrase
     }
     return existe;
 }
+public static void TraerDatosUsuario(string username){
+    string SQL = "SELECT * FROM Usuario WHERE UserName = @pUsuario "; 
+    using(SqlConnection db = new SqlConnection(_connectionString)){
+    //Usuario usuarioTraido = db.Query<Usuario>(SQL, new{pUsuario = username}); //Cómo convierto el tipo de dato a usuario?
+    //return usuarioTraido;
+}
+}
+public static bool BuscarSiExiste(string username){
+    string SQL = "SELECT UserName FROM Usuario WHERE UserName = @pUsuario "; 
+    using(SqlConnection db = new SqlConnection(_connectionString)){
+    string usuarioTraido = db.Query<Usuario>(SQL, new{pUsuario = username}).ToString();
+    bool ret = false;
+    if(usuarioTraido != null){
+        ret = true;
+    } 
+    return ret;
+}
+
 }
 
 public static string BuscarMail(string usuarioIngresado){
-    string SQL = "SELECT Mail FROM Usuario WHERE UserName = @pUsuario"; //If exists?
+    string SQL = "SELECT Mail FROM Usuario WHERE UserName = @pUsuario"; //If exists? Tira error sino?
     string mailTraido = String.Empty;
        using(SqlConnection db = new SqlConnection(_connectionString)){
     
@@ -34,3 +53,4 @@ public static string BuscarMail(string usuarioIngresado){
     return mailTraido;
 
        }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,23 +2,56 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
 function ValidarLogIn(){
+    return UsuarioYContraseña();
+}
+function ValidarRegistro(){
+    UsuarioYContraseña();
+    ContraseñasCoinciden();
+}
+function ContraseñasCoinciden(){
+    const password = document.getElementById('contraseña');
+    const passRepe = document.getElementById('password2');
+    if(!(password === passRepe)){
+        let error = document.getElementById('mostrarError');
+        error.innerHTML("Las contraseñas no coinciden");
+    }
+}
+
+function UsuarioYContraseña(){
     const username = document.getElementById('usuario');
     const password = document.getElementById('contraseña');
-    let tieneMayus = !(password === password.toLowerCase());
-    let tieneMinus = ValidarMinus(password);
-    if(password.length != 8){
-        let errorContraseña = document.getElementById('mostarError');
-        errorContraseña.innerHTML("Su contraseña no tiene los ocho caracteres necesarios");
+    let tieneMayus = ValidarMayus(password);
+    let tieneMinus = ValidarMinus();
+    let tiene8Min = ValidarExtension(password);
+    if(tiene8Min && tieneMinus && tieneMayus){
+        document.getElementById('loginForm').submit(); //Hay que poner esto o se envia directo?
+        //Si hay que ponerlo habria que poner en la instruccion de arriba una varible para que se pueda llamar desde != funciones
+        
     }else{
-        document.getElementById('loginForm').submit();
+        let errorContraseña = document.getElementById('mostarError');
+        errorContraseña.innerHTML("Su contraseña no cumple con los requisitos necesarios (al menos 8 digitos, 1 mayúscula y 1 minúscula)");
+    }
+    if(ValidarUsuario()){ //Es necesario?
+        let errorUsuario = document.getElementById('mostarError');
+        errorUsuario.innerHTML("Usuario no completado");
     }
     // https://es.stackoverflow.com/questions/411752/como-puedo-enviar-formulario-tras-validar-con-javascript 
 }
 
-function ValidarUsuario(){
+function ValidarUsuario(username){
     const username = document.getElementById('usuario');
+    let bool = false;
+    if(username.trim() != ""){
+         bool = true;
+    }
+    return bool;
+}
+
+function ValidarCodigo(){
+    const codigoIngresado = document.getElementById('codigoIngresado');
+    const codigoGenerado = document.getElementById('codigoGenerado');
+    return (codigoIngresado === codigoGenerado);
+
     
 }
