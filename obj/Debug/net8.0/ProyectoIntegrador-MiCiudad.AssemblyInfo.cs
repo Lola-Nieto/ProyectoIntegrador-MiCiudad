cdd9845ae24d195d7b141a70557fd331fffb3ba1
DANTE OKSENIUK <48313641@ort>
2024-11-13 10:27:41 -0300

    etesech

--- obj/Debug/net8.0/ProyectoIntegrador-MiCiudad.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoIntegrador-MiCiudad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14c3deb9067cab11c62f48c1cd2365c37661f6fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e605d98b063049fb043dfa3b8f84e8b0d5cf33")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoIntegrador-MiCiudad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
