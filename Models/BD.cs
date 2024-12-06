56f5c064ea92c3661a2f26f94328cd25edc90eda
Lola-Nieto <48588470@est.ort.edu.ar>
2024-12-05 23:59:52 -0300

    Update BD.cs

--- Models/BD.cs
+++ Models/BD.cs
@@ -8,7 +8,7 @@ using ProyectoIntegrador_MiCiudad.Models.ModelsViews;
 namespace ProyectoIntegrador_MiCiudad.Models;
 
 public static class BD{
-private static string _connectionString = @"Server=LMNHCOMPU\SQLEXPRESS02;Database=MiCiudad; Trusted_Connection=True";
+private static string _connectionString = @"Server=.;Database=MiCiudad; Trusted_Connection=True";
 
 
 public static void AgregarVecino(Usuario userAAgregar) {
